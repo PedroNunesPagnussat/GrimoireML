98ccc62d7bf2306157f737c07fb7c894db3ba19f
nppedronp@outlook.com <nppedronp@outlook.com>
2023-08-31 23:27:25 -0300

    dbscan with efficient neigborhs method

--- grimoireml/cluster/dbscan.py
+++ grimoireml/cluster/dbscan.py
@@ -10,7 +10,7 @@ class DBSCAN():
             self.data_point = data_point
             self.cluster = -1
             self.visited = False
-            self.neighbors = None
+
 
     def __init__(self, eps=0.5, min_points=5, dist_func="euclidean"):
         """Initialize DBSCAN with epsilon and minimum points."""
@@ -19,11 +19,12 @@ class DBSCAN():
         self._points = None
         self.dist_func = distance_functions.get_distance_function(dist_func)
         self.n_clusters = 0
+        self.points_array = None
 
     def fit(self, data):
         """Fit the model to the data."""
         self._points = np.array([self.Point(point) for point in data])
-        self._set_neighbors()
+        self.points_array = np.array([p.data_point for p in self._points]) 
         self._dbscan()
         return self.get_clusters()
 
@@ -37,49 +38,35 @@ class DBSCAN():
             if point.visited:
                 continue
             point.visited = True
-            neighbors = point.neighbors
+            neighbors = self._get_neighbors(point)             
             if len(neighbors) < self._min_points:
                 continue
 
             self._expand_cluster(point, neighbors)
             self.n_clusters += 1
 
-    def _set_neighbors(self):
-        """Pre-calculate neighbors for all points."""
-        points_array = np.array([p.data_point for p in self._points])
-        for point in self._points:
-            point.neighbors = self._get_neighbors(point, points_array)
-
-    def _get_neighbors(self, point, points_array):
+    def _get_neighbors(self, point):
         """Get neighbors of a given point."""
-        
-        distances = np.linalg.norm(points_array - point.data_point, axis=1)
+        distances = self.dist_func(self.points_array, point.data_point)
         distances[np.isclose(distances, 0)] = float('inf')
         neighbor_indices = np.where(distances <= self._eps)[0]
         return [self._points[i] for i in neighbor_indices]
     
-    def _expand_cluster_(self, point, neighbors):
-        """Expand the cluster from a given point."""
-        point.cluster = self.n_clusters
-        point.visited = True
-        for n in neighbors:
-            if not n.visited:
-                n.visited = True
-                n.cluster = self.n_clusters
-                new_neighbors = n.neighbors
-                if len(new_neighbors) > self._min_points:
-                    self._expand_cluster(n, new_neighbors)
 
 
     def _expand_cluster(self, point, neighbors):
+        """Expand the cluster from a given point."""
+        unique_neighbors = set(neighbors)
+        
         point.cluster = self.n_clusters
         point.visited = True
 
-        while len(neighbors) > 0:
-            n = neighbors.pop()
+        while unique_neighbors:
+            n = unique_neighbors.pop()
             if not n.visited:
                 n.visited = True
                 n.cluster = self.n_clusters
-                new_neighbors = n.neighbors
+                new_neighbors = self._get_neighbors(n)
+
                 if len(new_neighbors) > self._min_points:
-                    neighbors.extend(new_neighbors)
+                    unique_neighbors.update(new_neighbors)
--- grimoireml/functions/distance_functions.py
+++ grimoireml/functions/distance_functions.py
@@ -14,6 +14,7 @@ def get_distance_function(distance):
 def _euclidean_distance(x, y):
     """Calculate Euclidean distance between two vectors x and y."""
     # Using numpy's built-in function for efficiency
+
     return np.linalg.norm(x - y, axis=1)
 
 def _manhattan_distance(x, y):
