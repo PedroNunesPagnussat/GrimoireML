ff92500e68cc9f26cabe8f6f7d2f5291588bea47
nppedronp@outlook.com <nppedronp@outlook.com>
2023-09-15 18:38:19 -0300

    function abstract class

---
+++ grimoireml/src/Functions/function.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class Function(ABC):
+    """This is the base class for all functions"""
+
+    @abstractmethod
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        """This method is what calculates the function value at a given point"""
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        """This method is called when the function is printed"""
+        pass
+
